e02bb5fa9cf1add63c7d6701458ae653f8329b02
Pedrum <pedrum.jalali@gmail.com>
2017-02-15 16:11:01 -0500

    f

--- Server.java
+++ Server.java
@@ -9,13 +9,17 @@ public class Server {
 
     public static int GetServerIP(dbParams __dbParams) throws SQLException{
         SqlConnect sqlConnect = new SqlConnect(__dbParams);
-        String _query = "Select count(*) as count, Dest from " + __dbParams.dbName + ".labels "  +
-                "group by Dest " +
-                "order by count desc " +
-                "limit 1";
+        String _query = "select count(*), dest from\n" +
+                "(\n" +
+                "SELECT * FROM "+__dbParams.dbName+".labels\n" +
+                "where tag <> \"Normal\"\n" +
+                ") as t\n" +
+                "group by dest\n" +
+                " order by count(*) desc" +
+                " limit 1";
         ResultSet _rs = sqlConnect.executeQuery(_query);
         if (_rs.next()){
-            return (int)sqlConnect.getValAtIndex(_rs, "Dest", 0, SqlConnect.TYPE.INT );
+            return (int)sqlConnect.getValAtIndex(_rs, "dest", 0, SqlConnect.TYPE.INT );
         }
         else{
             return -1;
