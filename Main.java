6507550dd18611809fc152b46dd9a079dfdd21ac
Pedrum <pedrum.jalali@gmail.com>
2017-02-14 21:00:48 -0500

    implemnted count feature

--- Main.java
+++ Main.java
@@ -2,25 +2,16 @@ import DateTime.DateTime;
 import MySqlJava.dbParams;
 
 public class Main {
-
-
-    static  Queries _queries;
-    public static int _COUNTROWS = 25200;
+    static  InitCountTable _init;
 
     public static void main(String[] args) throws Exception {
 
-        DateTime _minTime;
-        DateTime _maxTime;
         dbParams _dbParams = new dbParams("192.168.20.12", "fafdRE$3", "D12" );
-        _queries = new Queries(_dbParams);
-        _queries.createConnectionTable(1);
-        _minTime =_queries.getMinTime();
-        _maxTime = _queries.getMaxTime();
-        _queries.setConnectionRows(_minTime, _maxTime);
+        _init = new InitCountTable(_dbParams);
+        _init.createConnectionTable(1);
+
+        _init.setConnectionRows();
+
+
     }
-
-
-
-
-
 }
