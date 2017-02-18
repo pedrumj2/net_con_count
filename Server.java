324579057a6113e0b1bc9a3b51cecf61950f5932
Pedrum <pedrum.jalali@gmail.com>
2017-02-18 17:33:43 -0500

    added the option of modeling clean traffic only

--- Server.java
+++ Server.java
@@ -7,16 +7,29 @@ import java.sql.SQLException;
 
 public class Server {
 
-    public static int GetServerIP(dbParams __dbParams) throws SQLException{
+    public static int GetServerIP(dbParams __dbParams, boolean __isAttack) throws SQLException{
         SqlConnect sqlConnect = new SqlConnect(__dbParams);
-        String _query = "select count(*), dest from\n" +
-                "(\n" +
-                "SELECT * FROM "+__dbParams.dbName+".labels\n" +
-                "where tag <> \"Normal\"\n" +
-                ") as t\n" +
-                "group by dest\n" +
-                " order by count(*) desc" +
-                " limit 1";
+        String _query;
+        if (__isAttack){
+            _query =  "select count(*), dest from\n" +
+                    "(\n" +
+                    "SELECT * FROM "+__dbParams.dbName+".labels\n" +
+                    "where tag <> \"Normal\"\n" +
+                    ") as t\n" +
+                    "group by dest\n" +
+                    " order by count(*) desc" +
+                    " limit 1";
+        }
+        else{
+            _query =  "select count(*), dest from\n" +
+                    "(\n" +
+                    "SELECT * FROM "+__dbParams.dbName+".labels\n" +
+                    ") as t\n" +
+                    "group by dest\n" +
+                    " order by count(*) desc" +
+                    " limit 1";
+        }
+
         ResultSet _rs = sqlConnect.executeQuery(_query);
         if (_rs.next()){
             return (int)sqlConnect.getValAtIndex(_rs, "dest", 0, SqlConnect.TYPE.INT );
